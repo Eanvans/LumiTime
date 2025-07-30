3b86adafff983f25396a2741f7c3b4b54c7f672f
Eanvans <66707924+Eanvans@users.noreply.github.com>
2025-07-30 21:59:40 +0800

    update

--- scheduleData_1753883939.js
+++ scheduleData_1753883939.js
@@ -1,56 +1,68 @@
 ﻿const scheduleData =[
   {
-    "timestamp": 1753736400,
-    "platform": "Twitch",
-    "title": "Stream",
-    "link": "https://www.twitch.tv/kanekolumi"
-  },
-  {
-    "timestamp": 1753804800,
+    "timestamp": 1753113600,
     "platform": "YouTube",
     "title": "Stream",
     "link": "https://www.youtube.com/@KanekoLumi"
   },
   {
-    "timestamp": 1753840800,
+    "timestamp": 1753131600,
+    "platform": "Twitch",
+    "title": "Stream",
+    "link": "https://www.twitch.tv/kanekolumi"
+  },
+  {
+    "timestamp": 1753200000,
+    "platform": "YouTube",
+    "title": "Stream",
+    "link": "https://www.youtube.com/@KanekoLumi"
+  },
+  {
+    "timestamp": 1753221600,
+    "platform": "Twitch",
+    "title": "Collab with Clio and Miia",
+    "link": "https://www.twitch.tv/kanekolumi"
+  },
+  {
+    "timestamp": 1753236000,
     "platform": "Discord",
     "title": "Movie Night",
     "link": "https://www.twitch.tv/kanekolumi"
   },
   {
-    "timestamp": 1753894800,
+    "timestamp": 1753290000,
     "platform": "Twitch",
     "title": "Stream",
     "link": "https://www.twitch.tv/kanekolumi"
   },
   {
-    "timestamp": 1753981200,
+    "timestamp": 1753376400,
     "platform": "Twitch",
     "title": "Stream",
     "link": "https://www.twitch.tv/kanekolumi"
   },
   {
-    "timestamp": 1754064000,
+    "timestamp": 1753459200,
     "platform": "YouTube",
     "title": "Stream",
     "link": "https://www.youtube.com/@KanekoLumi"
   },
   {
-    "timestamp": 1754082000,
+    "timestamp": 1753477200,
     "platform": "Twitch",
     "title": "Stream",
     "link": "https://www.twitch.tv/kanekolumi"
   },
   {
-    "timestamp": 1754100000,
-    "platform": "YouTube",
-    "title": "A/LURE Collab",
-    "link": "https://www.youtube.com/@KanekoLumi"
+    "timestamp": 1753545600,
+    "platform": "Twitch",
+    "title": "Strategy Saturday",
+    "link": "https://www.twitch.tv/kanekolumi"
   },
   {
-    "timestamp": 1754236800,
+    "timestamp": 1753632000,
     "platform": "Twitch",
-    "title": "Lumi being drawn and quartered by a Satsuma",
+    "title": "\u003Ca:LumiChaos:1295487558007914536\u003EThe Ninth Circle of Hell: Finland \u003Ca:LumiChaos:1295487558007914536\u003E",
     "link": "https://www.twitch.tv/kanekolumi"
   }
 ]
